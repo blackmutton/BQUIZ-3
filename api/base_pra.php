250ccbca3a2c245265913511432f6daab2438b15
yt Chen <yutongc30@gmail.com>
2024-07-12 10:44:18 +0800

    建立登入畫面

---
+++ api/base_pra.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+class DB
+{
+    protected $table;
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=dbno3";
+    protected $pdo;
+
+    public function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+    }
+    public function all(...$arg)
+    {
+        $sql = "select * from `$this->table`";
+
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->a2s($arg[0]);
+                $sql .= "where " . join(" && ", $tmp);
+            } else {
+                $sql .= $arg[0];
+            }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        echo $sql;
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function find($arg)
+    {
+        $sql = "slect * from `$this->table`";
+    }
+}
