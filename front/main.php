124dd82d41a1869e1d8604e95736aac46c51d907
yt Chen <yutongc30@gmail.com>
2024-07-15 09:12:24 +0800

    建立前台海報區版面

--- front/main.php
+++ front/main.php
@@ -1,22 +1,26 @@
 <div class="half" style="vertical-align:top;">
-        <h1>預告片介紹</h1>
-        <div class="rb tab" style="width:95%;">
-          <div id="abgne-block-20111227">
-            <ul class="lists">
-            </ul>
-            <ul class="controls">
-            </ul>
-          </div>
+  <h1>預告片介紹</h1>
+  <div class="rb tab" style="width:95%;">
+    <div id="posters">
+      <div class="lists">
+        <div class='poster'>
+          <img src="./images/03A07.jpg">
         </div>
+        <div class='name'>預告片1</div>
       </div>
-      <div class="half">
-        <h1>院線片清單</h1>
-        <div class="rb tab" style="width:95%;">
-          <table>
-            <tbody>
-              <tr> </tr>
-            </tbody>
-          </table>
-          <div class="ct"> </div>
-        </div>
+      <div class="controls">
       </div>
+    </div>
+  </div>
+</div>
+<div class="half">
+  <h1>院線片清單</h1>
+  <div class="rb tab" style="width:95%;">
+    <table>
+      <tbody>
+        <tr> </tr>
+      </tbody>
+    </table>
+    <div class="ct"> </div>
+  </div>
+</div>
