c46ece5ed5757348122c227602406dda3f9f7c3d
yt Chen <yutongc30@gmail.com>
2024-07-16 11:54:34 +0800

    增加編輯院線片表單及建立按鈕導向

--- backend/movie.php
+++ backend/movie.php
@@ -32,7 +32,7 @@
         width: 33%;
     }
 </style>
-<button>新增電影</button>
+<button onclick="location.href='?do=add_movie'">新增電影</button>
 <hr>
 <div class="movie">
     <div class="img">
@@ -51,7 +51,7 @@
             <button>顯示</button>
             <button>往上</button>
             <button>往下</button>
-            <button>編輯電影</button>
+            <button onclick="location.href='?do=edit_movie'">編輯電影</button>
             <button>刪除電影</button>
         </div>
         <div class="intro">
