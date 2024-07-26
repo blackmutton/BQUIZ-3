99fcff52f885ceb27405f5c1c80fa0859a493190
yt Chen <yutongc30@gmail.com>
2024-07-26 10:47:34 +0800

    建立前台院線片列表及分頁功能

--- front/main.php
+++ front/main.php
@@ -127,11 +127,34 @@
 <div class="half">
   <h1>院線片清單</h1>
   <div class="rb tab" style="width:95%;">
-    <table>
-      <tbody>
-        <tr> </tr>
-      </tbody>
-    </table>
-    <div class="ct"> </div>
+    <?php
+    $all = $Movie->count(" where ondate >= '2024-07-26' && sh=1");
+    $div = 4;
+    $pages = ceil($all / $div);
+    $now = $_GET['p'] ?? 1;
+    $start = ($now - 1) * $div;
+    $movies = q("select * from `movies` where ondate >= '2024-07-26' && sh=1 order by rank limit $start,$div");
+    foreach ($movies as $movie) {
+    ?>
+      <div class="movie"><?= $movie['name'] ?></div>
+    <?php
+    }
+    ?>
+    <div class="ct">
+      <?php
+      if (($now - 1) > 0) {
+        $prev = $now - 1;
+        echo "<a href='?p=$prev'> <</a>";
+      }
+      for ($i = 1; $i <= $pages; $i++) {
+        $size = ($i == $now) ? 'font-size:18px' : '';
+        echo "<a href='?p-$i' style='$size'>$i</a>";
+      }
+      if (($now + 1) <= $pages) {
+        $next = $now + 1;
+        echo "<a href='?p=$next'> ></a>";
+      }
+      ?>
+    </div>
   </div>
 </div>
