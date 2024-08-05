924a8bd76fa8b45c92bef4789e285fe196b32da6
yt Chen <yutongc30@gmail.com>
2024-08-05 10:57:04 +0800

    建立訂單結果畫面

--- api/order.php
+++ api/order.php
@@ -19,7 +19,7 @@ $_POST['no'] = date("Ymd") . sprintf("%04d", $no);
 //     [id] => 2
 //     [date] => 2024-08-05
 //     [session] => 14:00~16:00
-//     [seat] => Array
+//     [seats] => Array
 //         (
 //             [0] => 4
 //             [1] => 9
@@ -40,7 +40,7 @@ unset($_POST['id']);
 
 $Order->save($_POST);
 
-dd($_POST);
+// dd($_POST);
 // Array
 // (
 //     [date] => 2024-08-05
@@ -50,3 +50,59 @@ dd($_POST);
 //     [movie] => 院線112
 //     [qt] => 2
 // )
+?>
+<style>
+    #orderResult {
+        width: 500px;
+        margin: auto;
+        border: 1px solid #999;
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    #orderResult tr:nth-child(even) {
+        background: #eee;
+    }
+
+    #orderResult td {
+        padding: 5px;
+    }
+</style>
+<table id="orderResult">
+    <tr>
+        <td colspan="2">感謝您的訂購，您的訂單編號是:<?= $_POST['no'] ?></td>
+        <!-- <td></td> -->
+    </tr>
+    <tr>
+        <td>電影名稱:</td>
+        <td><?= $_POST['movie'] ?></td>
+    </tr>
+    <tr>
+        <td>日期:</td>
+        <td><?= $_POST['date'] ?></td>
+    </tr>
+    <tr>
+        <td>場次時間:</td>
+        <td><?= $_POST['session'] ?></td>
+    </tr>
+    <tr>
+        <td colspan="2">
+            座位: <br>
+            <?php
+            $seats = unserialize($_POST['seats']);
+            foreach ($seats as $seat) {
+                echo (floor($seat / 5) + 1) . "排" . ($seat % 5 + 1) . "號";
+                echo "<br>";
+            }
+            ?>
+            共<?= $_POST['qt'] ?>
+        </td>
+        <!-- <td></td> -->
+    </tr>
+    <tr>
+        <td colspan="2">
+            <button onclick="location.href='index.php'">確認</button>
+        </td>
+        <!-- <td></td> -->
+    </tr>
+</table>
